a88a495ebbfbff768d9081f7979f684ec8d8918a
Steven Marshall <SMarshall@chef.io>
2020-06-22 17:10:33 +0100

    [libedit] Various refactors to existing code
    Added inspec.lock to .gitignore
    Updated README.md to latest binary format
    Added plan_name to attributes.yml
    Updated controls to better format
    Updated inspec.yml to latest version
    
    Signed-off-by: Steven Marshall <SMarshall@chef.io>

--- controls/libedit_exists.rb
+++ controls/libedit_exists.rb
@@ -8,30 +8,35 @@ control 'core-plans-libedit' do
   impact 1.0
   title 'Ensure libedit libraries exist as expected'
   desc '
-  We check that the three folders that libedit installs are present.
+  To test libedit libraries exists we check for the path of the installed package.
+  Then we check that the library exists in the expected location.  
+    $ ls -al $PKG_PATH/include
+       
   '
 
   hab_pkg_path = command("hab pkg path #{plan_ident}")
   describe hab_pkg_path do
-    its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
-  end
-
-  describe command("ls #{File.join(hab_pkg_path.stdout.strip, "include")}") do
-    its('stdout') { should_not be_empty }
-    its('stderr') { should eq '' }
+    its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 
-  describe command("ls #{File.join(hab_pkg_path.stdout.strip, "lib")}") do
+  describe command("ls -al #{File.join(hab_pkg_path.stdout.strip, "include")}") do
     its('stdout') { should_not be_empty }
-    its('stderr') { should eq '' }
+    its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
 
-  describe command("ls #{File.join(hab_pkg_path.stdout.strip, "share")}") do
+  describe command("ls -al #{File.join(hab_pkg_path.stdout.strip, "lib")}") do
     its('stdout') { should_not be_empty }
-    its('stderr') { should eq '' }
+    its('stderr') { should be_empty }
     its('exit_status') { should eq 0 }
   end
+
+  describe command("ls -al #{File.join(hab_pkg_path.stdout.strip, "share")}") do
+    its('stdout') { should_not be_empty }
+    its('stderr') { should be_empty }
+    its('exit_status') { should eq 0 }
+  end
+
 end
