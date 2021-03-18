1eea792702bfc8ff0ed18e61a7383e35ac871617
Lucas Giancola <lucas.giancola@live.com>
2021-03-18 11:07:24 -0400

    Bumped version 3.1.20150325 -> 3.1.20210216

--- plan.sh
+++ plan.sh
@@ -1,13 +1,13 @@
 pkg_name=libedit
 pkg_origin=core
-pkg_version=3.1.20150325
+pkg_version=3.1.20210216
 pkg_license=('BSD-3-Clause')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=http://thrysoee.dk/editline/libedit-20150325-3.1.tar.gz
+pkg_source=http://thrysoee.dk/editline/libedit-20210216-3.1.tar.gz
 pkg_dirname="${pkg_name}-20150325-3.1"
 pkg_upstream_url="https://thrysoee.dk/editline/"
 pkg_description="This is an autotool- and libtoolized port of the NetBSD Editline library (libedit)."
-pkg_shasum=c88a5e4af83c5f40dda8455886ac98923a9c33125699742603a88a0253fcc8c5
+pkg_shasum=2283f741d2aab935c8c52c04b57bf952d02c2c02e651172f8ac811f77b1fc77a
 pkg_deps=(core/glibc core/ncurses)
 pkg_build_deps=(core/gcc core/make core/coreutils)
 pkg_lib_dirs=(lib)
